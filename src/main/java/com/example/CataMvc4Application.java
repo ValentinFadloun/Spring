221a9c06ebac7525d1c53feccac905ccd568e60b
Valentin Fadloun <v.fadloun@gmail.com>
2020-03-17 17:02:30 +0100

    journee 2 teletravail - login

--- src/main/java/com/example/CataMvc4Application.java
+++ src/main/java/com/example/CataMvc4Application.java
@@ -11,10 +11,7 @@ public class CataMvc4Application {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(CataMvc4Application.class, args);
-		ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
-		produitRepository.save(new Produit("LX 4352", 670, 90));
-		produitRepository.save(new Produit("Ord HP 432", 670, 90));
-		
+		ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);		
 		produitRepository.findAll().forEach((p) -> System.out.println(p.getDesignation()));
 	}
 
--- src/main/java/com/example/sec/SecurityConfig.java
+++ src/main/java/com/example/sec/SecurityConfig.java
@@ -1,32 +1,40 @@
 package com.example.sec;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @Configuration @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		/*
-		auth.inMemoryAuthentication().withUser("admin").password("123").roles("USER", "ADMIN");
-		auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");*/
-		auth.inMemoryAuthentication().withUser("simm").password("123").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("123").roles("USER", "ADMIN");
+		/*auth.inMemoryAuthentication().withUser("simm").password("{noop}123").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}123").roles("USER", "ADMIN");*/
+		auth.jdbcAuthentication().dataSource(dataSource)
+			.usersByUsernameQuery("SELECT login as principal, pass as credentials, active FROM users WHERE login=?")
+			.authoritiesByUsernameQuery("SELECT login as principal, role FROM usersroles WHERE login=?")
+			.passwordEncoder(new BCryptPasswordEncoder())
+			.rolePrefix("ROLE_");
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		http.formLogin();
-		
-		http.authorizeRequests().antMatchers("/index").hasRole("USER");
-		http.authorizeRequests().antMatchers("/form","/save","/edit","/delete").hasRole("ADMIN");
+		http.formLogin().loginPage("/login");
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/user/*").hasRole("USER");
+		http.authorizeRequests().antMatchers("/admin/*").hasRole("ADMIN");
+		http.exceptionHandling().accessDeniedPage("/403");
 	}
 }
